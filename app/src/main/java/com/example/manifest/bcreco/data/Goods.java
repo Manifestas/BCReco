386eb3700ad7ffde99857969195b37841ef547f2
tv.unimol <doslovno@gmail.com>
2017-10-15 19:43:07 +0300

    add Goods class

---
+++ app/src/main/java/com/example/manifest/bcreco/data/Goods.java
@@ -0,0 +1,16 @@
+package com.example.manifest.bcreco.data;
+
+public class Goods {
+
+    private String barcode;
+    private String model;
+    private String color;
+    private String size;
+
+    public Goods(String barcode, String model, String color, String size) {
+        this.barcode = barcode;
+        this.model = model;
+        this.color = color;
+        this.size = size;
+    }
+}
