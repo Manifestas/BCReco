7d7d53409aef9137344b686a2a7ff3a900f7e5cf
manifest <doslovno@gmail.com>
2017-11-20 01:05:20 +0300

    add settings menu

--- app/src/main/java/com/example/manifest/bcreco/MainActivity.java
+++ app/src/main/java/com/example/manifest/bcreco/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -74,6 +76,25 @@ public class MainActivity extends Activity {
                 new GoodsDBAsyncTask().execute(barcodeString);
             }
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        // Return true so that menu is displayed in the toolbar
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        // When Settings menu item is pressed, open SettingActivity
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private class GoodsDBAsyncTask extends AsyncTask<String, Void, Goods> {
