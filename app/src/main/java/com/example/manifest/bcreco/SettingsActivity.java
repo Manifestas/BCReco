4bda852aca838a4a0a8322c9c0419ec09c826b45
tv.unimol <doslovno@gmail.com>
2018-12-02 17:28:37 +0300

    enable toolbar in SettingsActivity

--- app/src/main/java/com/example/manifest/bcreco/SettingsActivity.java
+++ app/src/main/java/com/example/manifest/bcreco/SettingsActivity.java
@@ -1,8 +1,11 @@
 package com.example.manifest.bcreco;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NavUtils;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        ActionBar actionBar = getSupportActionBar();
+        // Set the action bar back button to look like an up button
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        //when home button is pressed - take back to MainActivity
+        if (id == android.R.id.home) {
+            NavUtils.navigateUpFromSameTask(this);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
