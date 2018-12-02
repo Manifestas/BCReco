d83b0e9b974ec3862c7370a065c7da84409f08e3
tv.unimol <doslovno@gmail.com>
2018-12-02 21:13:07 +0300

    set summary for EditTextPreference

--- app/src/main/java/com/example/manifest/bcreco/SettingsFragment.java
+++ app/src/main/java/com/example/manifest/bcreco/SettingsFragment.java
@@ -1,13 +1,56 @@
 package com.example.manifest.bcreco;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.preference.EditTextPreference;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceScreen;
 
-public class SettingsFragment extends PreferenceFragmentCompat {
+public class SettingsFragment extends PreferenceFragmentCompat
+        implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+    }
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.preference);
+
+        SharedPreferences sharedPreferences = getPreferenceScreen().getSharedPreferences();
+        PreferenceScreen prefScreen = getPreferenceScreen();
+        int count = prefScreen.getPreferenceCount();
+
+        // set summary for all EditTextPreference
+        for (int i = 0; i < count; i++) {
+            Preference p = prefScreen.getPreference(i);
+            if (p instanceof EditTextPreference) {
+                String value = sharedPreferences.getString(p.getKey(), "");
+                p.setSummary(value);
+            }
+        }
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        // which preference was changed
+        Preference preference = findPreference(key);
+        if (preference != null) {
+            if (preference instanceof EditTextPreference) {
+                // set summary equals preference value
+                String value = sharedPreferences.getString(preference.getKey(), "");
+                preference.setSummary(value);
+            }
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
     }
 }
