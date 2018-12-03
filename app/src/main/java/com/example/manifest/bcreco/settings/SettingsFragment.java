47b4c670ea014d9bb7666f9b92aa862e0b23ef1b
tv.unimol <doslovno@gmail.com>
2018-12-03 12:45:16 +0300

    add validate input in preferences

--- app/src/main/java/com/example/manifest/bcreco/settings/SettingsFragment.java
+++ app/src/main/java/com/example/manifest/bcreco/settings/SettingsFragment.java
@@ -2,8 +2,10 @@ package com.example.manifest.bcreco.settings;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.manifest.bcreco.R;
+import com.example.manifest.bcreco.data.DbUtils;
 
 import androidx.preference.EditTextPreference;
 import androidx.preference.Preference;
@@ -11,7 +13,8 @@ import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.PreferenceScreen;
 
 public class SettingsFragment extends PreferenceFragmentCompat
-        implements SharedPreferences.OnSharedPreferenceChangeListener {
+        implements SharedPreferences.OnSharedPreferenceChangeListener,
+        Preference.OnPreferenceChangeListener {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -35,6 +38,13 @@ public class SettingsFragment extends PreferenceFragmentCompat
                 p.setSummary(value);
             }
         }
+
+        // for validate correct input
+        int[] prefKeys = {R.string.pref_ip_key, R.string.pref_port_key, R.string.pref_store_id_key};
+        for (int key: prefKeys) {
+            Preference preference = findPreference(getString(key));
+            preference.setOnPreferenceChangeListener(this);
+        }
     }
 
     @Override
@@ -51,8 +61,38 @@ public class SettingsFragment extends PreferenceFragmentCompat
     }
 
     @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        String preferenceKey = preference.getKey();
+        if (preferenceKey.equals(getString(R.string.pref_ip_key))) {
+            String ipAddress = (String) newValue;
+            if (!DbUtils.isIPV4(ipAddress)) {
+                showErrorToast(getString(R.string.toast_message_incorrect_ip_address));
+                return false;
+            }
+        } else if (preferenceKey.equals(getString(R.string.pref_port_key))) {
+            String port = (String) newValue;
+            if (!DbUtils.isPortValid(port)) {
+                showErrorToast(getString(R.string.toast_message_incorrect_port));
+                return false;
+            }
+        } else if (preferenceKey.equals(getString(R.string.pref_store_id_key))) {
+            String storeId = (String) newValue;
+            if (!DbUtils.isStoreIdValid(storeId)) {
+                showErrorToast(getString(R.string.toast_message_incorrect_store_id));
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
     public void onDestroy() {
         super.onDestroy();
         getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
     }
+
+    private void showErrorToast(String toastMessage) {
+        Toast.makeText(getContext(), toastMessage, Toast.LENGTH_SHORT).show();
+    }
+
 }
