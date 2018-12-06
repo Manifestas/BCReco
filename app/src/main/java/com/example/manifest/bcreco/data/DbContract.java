62fd7002e89003a137d8d55afb796959aa509021
tv.unimol <doslovno@gmail.com>
2018-12-06 19:26:34 +0300

    change queryPluQty for more returning columns and clean class

--- app/src/main/java/com/example/manifest/bcreco/data/DbContract.java
+++ app/src/main/java/com/example/manifest/bcreco/data/DbContract.java
@@ -4,14 +4,10 @@ import android.provider.BaseColumns;
 
 public final class DbContract {
 
-    private DbContract(){}
-
     /**
      * Database name.
      */
     public static final String DB_NAME = "TradeX";
-
-    //TODO: move this to the SharedPreference and ask a user to enter log/pass in settings menu
     /**
      * Default user login.
      */
@@ -33,81 +29,48 @@ public final class DbContract {
      */
     public static final String UNIMOLL_ID = "119";
     public static final String DB_IP_ADDRESS = "192.168.14.2:1433";
-
-    /**
-     * Connection url.
-     */
-    public static final String DB_CONN_URL = "jdbc:jtds:sqlserver://"
-                                             + DB_IP_ADDRESS + ";"
-                                             + "databaseName=" + DB_NAME
-                                             + ";user=" + DB_LOGIN
-                                             + ";password=" + DB_PASSWORD + ";";
-
     /**
      * Connection url.
      * %1$s - IP, %2$s - port, %3$s - login, %4$s - password.
      */
-    public static final String DB_CONN_URL_FROM_PREF = "jdbc:sqlserver://%1$s:%2$s;databaseName="
+    public static final String DB_CONN_URL_FROM_PREF = "jdbc:jtds:sqlserver://%1$s:%2$s;databaseName="
             + DB_NAME
             + ";user=%3$s;password=%4$s;";
-    /** SQL statement. */
-    public static final String INNER_JOIN = " INNER JOIN ";
-    /** SQL keyword. */
-    public static final String ON = " ON ";
-    /** A dot for sql command. */
-    public static final String DOT = ".";
-    /** A comma separator. */
-    public static final String COMMA = ", ";
-    /** A equal symbol. */
-    public static final String EQUALS = " = ";
-
-    /** Query from TradeX table for getting Goods parameters.
-     * Currency price, model name, model desc, size name,
-     * color name, season name, current exchange rate.
-     */
-    public static String  goodsQuery(String barcode) {
-        return "SELECT "
-                + PluEntry.TABLE_NAME + DOT + PluEntry.COLUMN_CURRENT_PRICE + COMMA
-                + ModelEntry.TABLE_NAME + DOT + ModelEntry.COLUMN_MODEL + COMMA
-                + ModelEntry.TABLE_NAME + DOT + ModelEntry.COLUMN_MODEL_DESC + COMMA
-                + SizeEntry.TABLE_NAME + DOT + SizeEntry.COLUMN_SIZE_NAME + COMMA
-                + ColorEntry.TABLE_NAME + DOT + ColorEntry.COLUMN_COLOR + COMMA
-                + SeasonEntry.TABLE_NAME + DOT + SeasonEntry.COLUMN_SEASON + COMMA
-                + ExchangeEntry.TABLE_NAME + DOT + ExchangeEntry.COLUMN_EXCHANGE_RATE
-            + " FROM " + BarcodeEntry.TABLE_NAME
-                + INNER_JOIN  + PluEntry.TABLE_NAME
-                + ON + BarcodeEntry.TABLE_NAME + DOT + BarcodeEntry.COLUMN_ID_PLU
-                + EQUALS + PluEntry.TABLE_NAME + DOT + PluEntry.COLUMN_ID
-                + INNER_JOIN + ModelEntry.TABLE_NAME
-                + ON + PluEntry.TABLE_NAME + DOT + PluEntry.COLUMN_ID_MODEL
-                + EQUALS + ModelEntry.TABLE_NAME + DOT + ModelEntry.COLUMN_ID
-                + INNER_JOIN + SizeEntry.TABLE_NAME
-                + ON + PluEntry.TABLE_NAME + DOT + PluEntry.COLUMN_ID_SIZE
-                + EQUALS + SizeEntry.TABLE_NAME + DOT + SizeEntry.COLUMN_SIZE_ID
-                + INNER_JOIN + ColorEntry.TABLE_NAME
-                + ON + PluEntry.TABLE_NAME + DOT + PluEntry.COLUMN_COLOR
-                + EQUALS + ColorEntry.TABLE_NAME + DOT + ColorEntry.COLUMN_COLOR_ID
-                + INNER_JOIN + SeasonEntry.TABLE_NAME
-                + ON + ModelEntry.TABLE_NAME + DOT + ModelEntry.COLUMN_SEASON_ID
-                + EQUALS + SeasonEntry.TABLE_NAME + DOT + SeasonEntry.COLUMN_SEASON_ID
-                + INNER_JOIN + ExchangeEntry.TABLE_NAME
-                + ON + ModelEntry.TABLE_NAME + DOT + ModelEntry.COLUMN_CURRENCY_ID
-                + EQUALS + ExchangeEntry.TABLE_NAME + DOT + ExchangeEntry.COLUMN_CURRENCY_ID
-            + " WHERE " + BarcodeEntry.TABLE_NAME + DOT +  BarcodeEntry.COLUMN_BARCODE
-                + EQUALS + "'" + BarcodeEntry.getValidBarcode(barcode) + "';";
-    }
-
     /**
-     * Query from TradeX table for getting Goods quantity passing id_plu.
+     * SQL statement.
+     */
+    public static final String INNER_JOIN = " INNER JOIN ";
+    /**
+     * SQL keyword.
+     */
+    public static final String ON = " ON ";
+    /**
+     * A dot for sql command.
+     */
+    public static final String DOT = ".";
+    /**
+     * A comma separator.
+     */
+    public static final String COMMA = ", ";
+    /**
+     * A equal symbol.
+     */
+    public static final String EQUALS = " = ";
+    /**
+     * Query from TradeX table for getting Goods and quantity passing id_plu.
      */
     public static final String queryPluQty =
             "SELECT "
-                    + PluEntry.TABLE_NAME + DOT + PluEntry.COLUMN_ID + COMMA
                     + ModelEntry.TABLE_NAME + DOT + ModelEntry.COLUMN_MODEL + COMMA
+                    + ModelEntry.TABLE_NAME + DOT + ModelEntry.COLUMN_MODEL_DESC + COMMA
                     + SizeEntry.TABLE_NAME + DOT + SizeEntry.COLUMN_SIZE_NAME + COMMA
                     + ColorEntry.TABLE_NAME + DOT + ColorEntry.COLUMN_COLOR + COMMA
                     + ObjectEntry.TABLE_NAME + DOT + ObjectEntry.COLUMN_OBJECT + COMMA
-                    + LogPluCostEntry.TABLE_NAME + DOT + LogPluCostEntry.COLUMN_QUANTITY
+                    + ObjectEntry.TABLE_NAME + DOT + ObjectEntry.COLUMN_OBJECT_SHORT_DESC + COMMA
+                    + LogPluCostEntry.TABLE_NAME + DOT + LogPluCostEntry.COLUMN_QUANTITY + COMMA
+                    + SeasonEntry.TABLE_NAME + DOT + SeasonEntry.COLUMN_SEASON + COMMA
+                    + PluEntry.TABLE_NAME + DOT + PluEntry.COLUMN_CURRENT_PRICE + COMMA
+                    + ExchangeEntry.TABLE_NAME + DOT + ExchangeEntry.COLUMN_EXCHANGE_RATE
                     + " FROM " + PluEntry.TABLE_NAME
                     + INNER_JOIN + ModelEntry.TABLE_NAME
                     + ON + PluEntry.TABLE_NAME + DOT + PluEntry.COLUMN_ID_MODEL
@@ -118,40 +81,52 @@ public final class DbContract {
                     + INNER_JOIN + ColorEntry.TABLE_NAME
                     + ON + PluEntry.TABLE_NAME + DOT + PluEntry.COLUMN_COLOR
                     + EQUALS + ColorEntry.TABLE_NAME + DOT + ColorEntry.COLUMN_COLOR_ID
+                    + INNER_JOIN + SeasonEntry.TABLE_NAME
+                    + ON + ModelEntry.TABLE_NAME + DOT + ModelEntry.COLUMN_SEASON_ID
+                    + EQUALS + SeasonEntry.TABLE_NAME + DOT + SeasonEntry.COLUMN_SEASON_ID
                     + INNER_JOIN + LogPluCostEntry.TABLE_NAME
                     + ON + PluEntry.TABLE_NAME + DOT + PluEntry.COLUMN_ID
                     + EQUALS + LogPluCostEntry.TABLE_NAME + DOT + LogPluCostEntry.COLUMN_ID_PLU
                     + INNER_JOIN + ObjectEntry.TABLE_NAME
                     + ON + LogPluCostEntry.TABLE_NAME + DOT + LogPluCostEntry.COLUMN_OBJECT_ID
                     + EQUALS + ObjectEntry.TABLE_NAME + DOT + ObjectEntry.COLUMN_OBJECT_ID
+                    + INNER_JOIN + ExchangeEntry.TABLE_NAME
+                    + ON + ModelEntry.TABLE_NAME + DOT + ModelEntry.COLUMN_CURRENCY_ID
+                    + EQUALS + ExchangeEntry.TABLE_NAME + DOT + ExchangeEntry.COLUMN_CURRENCY_ID
                     + " WHERE " + PluEntry.TABLE_NAME + DOT + PluEntry.COLUMN_ID_MODEL + EQUALS
                     + " (SELECT " + PluEntry.COLUMN_ID_MODEL
                     + " FROM " + PluEntry.TABLE_NAME
-                    + " WHERE " + PluEntry.TABLE_NAME + DOT + PluEntry.COLUMN_ID + EQUALS  + "? )";
+                    + " WHERE " + PluEntry.TABLE_NAME + DOT + PluEntry.COLUMN_ID + EQUALS + "? )";
+
+    private DbContract() {
+    }
 
     /**
      * Inner class that defines constant values for the barcode database table.
      */
     public static final class BarcodeEntry implements BaseColumns {
 
-        /** Name of database table for barcodes*/
+        /**
+         * Name of database table for barcodes
+         */
         public static final String TABLE_NAME = "T_Barcode";
 
         /**
          * Barcode number.
-         *
+         * <p>
          * Type: VARCHAR
          */
         public static final String COLUMN_BARCODE = "Barcode";
 
         /**
          * Internal code of the article (unique for sizes).
-         *
+         * <p>
          * Type: INT
          */
         public static final String COLUMN_ID_PLU = "ID_PLU";
 
         //TODO: add check sum.
+
         /**
          * For fast searching in database, it is necessary that the length
          * of the barcode string must be 12.
@@ -180,40 +155,42 @@ public final class DbContract {
      */
     public static final class PluEntry {
 
-        /** Name of database table for PLU*/
+        /**
+         * Name of database table for PLU
+         */
         public static final String TABLE_NAME = "T_PLU";
 
         /**
          * Internal code of the article (unique for sizes).
-         *
+         * <p>
          * Type: INT
          */
         public static final String COLUMN_ID = "ID";
 
         /**
          * ID of model.
-         *
+         * <p>
          * Type: INT
          */
         public static final String COLUMN_ID_MODEL = "ID_Models";
 
         /**
          * ID of color.
-         *
+         * <p>
          * Type: INT
          */
         public static final String COLUMN_COLOR = "ID_ColorVend";
 
         /**
          * ID of size.
-         *
+         * <p>
          * Type: INT
          */
         public static final String COLUMN_ID_SIZE = "ID_Sizes";
 
         /**
          * TradeX current price.
-         *
+         * <p>
          * Type: MONEY
          */
         public static final String COLUMN_CURRENT_PRICE = "RecomRetailPrice";
@@ -224,40 +201,42 @@ public final class DbContract {
      */
     public static final class ModelEntry {
 
-        /** Name of database table for PLU*/
+        /**
+         * Name of database table for PLU
+         */
         public static final String TABLE_NAME = "T_Models";
 
         /**
          * ID of model.
-         *
+         * <p>
          * Type: INT
          */
         public static final String COLUMN_ID = "ID";
 
         /**
          * Model name.
-         *
+         * <p>
          * Type: VARCHAR
          */
         public static final String COLUMN_MODEL = "ModelID";
 
         /**
          * Model description.
-         *
+         * <p>
          * Type: VARCHAR
          */
         public static final String COLUMN_MODEL_DESC = "ModelDesc";
 
         /**
          * ID currency of price(e.g. 3 - euro).
-         *
+         * <p>
          * Type: INT
          */
         public static final String COLUMN_CURRENCY_ID = "ExpCurrency";
 
         /**
          * ID of season.
-         *
+         * <p>
          * Type: INT
          */
         public static final String COLUMN_SEASON_ID = "Season";
@@ -268,19 +247,21 @@ public final class DbContract {
      */
     public static final class ColorEntry {
 
-        /** Name of database table for PLU*/
+        /**
+         * Name of database table for PLU
+         */
         public static final String TABLE_NAME = "T_ColorVend";
 
         /**
          * ID of color.
-         *
+         * <p>
          * Type: INT
          */
         public static final String COLUMN_COLOR_ID = "ID";
 
         /**
          * Color name(e.g. blue).
-         *
+         * <p>
          * Type: VARCHAR
          */
         public static final String COLUMN_COLOR = "Color";
@@ -298,14 +279,14 @@ public final class DbContract {
 
         /**
          * ID of season.
-         *
+         * <p>
          * Type: INT
          */
         public static final String COLUMN_SEASON_ID = "ID";
 
         /**
          * Season name(e.g. winter2017-18).
-         *
+         * <p>
          * Type: VARCHAR
          */
         public static final String COLUMN_SEASON = "RefDesc";
@@ -323,14 +304,14 @@ public final class DbContract {
 
         /**
          * ID of currency.
-         *
+         * <p>
          * Type: INT
          */
         public static final String COLUMN_CURRENCY_ID = "ID_Currency";
 
         /**
          * Exchange rate(e.g. 75 in the moment).
-         *
+         * <p>
          * Type: FLOAT
          */
         public static final String COLUMN_EXCHANGE_RATE = "ExchangeRate";
@@ -348,14 +329,14 @@ public final class DbContract {
 
         /**
          * ID of size(e.g. 12).
-         *
+         * <p>
          * Type: INT
          */
         public static final String COLUMN_SIZE_ID = "ID";
 
         /**
          * Size name(e.g. 39)
-         *
+         * <p>
          * Type: VARSHAR
          */
         public static final String COLUMN_SIZE_NAME = "SizeName";
@@ -373,7 +354,7 @@ public final class DbContract {
 
         /**
          * ID of object(e.g. 24033 (Unimoll)).
-         *
+         * <p>
          * Type: INT
          */
         public static final String COLUMN_OBJECT_ID = "ID";
@@ -385,7 +366,7 @@ public final class DbContract {
 
         /**
          * Short name of object(e.g. Юнимол)
-         *
+         * <p>
          * Type: VARSHAR
          */
         public static final String COLUMN_OBJECT_SHORT_DESC = "ShortDesc";
@@ -409,21 +390,21 @@ public final class DbContract {
 
         /**
          * Internal code of the article (unique for sizes).
-         *
+         * <p>
          * Type: INT
          */
         public static final String COLUMN_ID_PLU = "ID_PLU";
 
         /**
          * ID of object(e.g. 24033 (Unimoll)).
-         *
+         * <p>
          * Type: INT
          */
         public static final String COLUMN_OBJECT_ID = "ID_Objects";
 
         /**
          * Quantity of each unique ID of goods
-         *
+         * <p>
          * Type: FLOAT
          */
         public static final String COLUMN_QUANTITY = "StockQty";
