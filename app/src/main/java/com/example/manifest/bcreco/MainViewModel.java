27b0f041b464afc97f8c2de14611a96aa59d9f8d
tv.unimol <doslovno@gmail.com>
2018-12-07 13:52:08 +0300

    add loadProductFromDB

--- app/src/main/java/com/example/manifest/bcreco/MainViewModel.java
+++ app/src/main/java/com/example/manifest/bcreco/MainViewModel.java
@@ -1,6 +1,48 @@
 package com.example.manifest.bcreco;
 
-import androidx.lifecycle.ViewModel;
+import android.annotation.SuppressLint;
+import android.app.Application;
+import android.os.AsyncTask;
 
-public class MainViewModel extends ViewModel {
+import com.example.manifest.bcreco.data.DbConnectionParams;
+import com.example.manifest.bcreco.data.DbHelper;
+import com.example.manifest.bcreco.data.Product;
+import com.example.manifest.bcreco.settings.PrefHelper;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.MutableLiveData;
+
+public class MainViewModel extends AndroidViewModel {
+
+    private final MutableLiveData<Product> product = new MutableLiveData<>();
+    private DbConnectionParams connectionParams;
+
+    public MainViewModel(@NonNull Application application) {
+        super(application);
+        connectionParams = PrefHelper.getDbConnectionParams(application);
+    }
+
+    public MutableLiveData<Product> getProduct() {
+        return product;
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    private void loadProductFromDB(String barcode) {
+        new AsyncTask<String, Void, Product>() {
+            @Override
+            protected Product doInBackground(String... barcodes) {
+                // Don't perform the request if there are no barcodes, or the first barcode is null
+                if (barcodes.length < 1 || barcodes[0] == null) {
+                    return null;
+                }
+                return DbHelper.returnProductFromDb(connectionParams, barcodes[0]);
+            }
+
+            @Override
+            protected void onPostExecute(Product productFromDb) {
+                product.setValue(productFromDb);
+            }
+        };
+    }
 }
